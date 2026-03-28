7ec4d844c6e7d27f7ff01d6e16a20eeddc00ff85
Git-Balance <davidbond001@icloud.com>
2026-03-27 18:27:37 -0700

    Debugging and Love2d stuff

--- lua/custom/plugins/love.lua
+++ lua/custom/plugins/love.lua
@@ -8,5 +8,7 @@ return {
         { '<leader>v', ft = 'lua', desc = 'LÖVE' },
         { '<leader>vv', '<cmd>LoveRun<cr>', ft = 'lua', desc = 'Run LÖVE' },
         { '<leader>vs', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
+        -- Thanks this: https://github.com/S1M0N38/love2d.nvim/issues/11#issuecomment-3856994362
+        { '<leader>vl', '<cmd>lua vim.lsp.enable({ "lua_ls" }, false) vim.lsp.enable({ "lua_ls" })<cr>', ft = 'lua', desc = 'Restart LSP' },
     },
 }
